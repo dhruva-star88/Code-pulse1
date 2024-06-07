b5318797b0f61572b3ab33b463a6ed9cab8d8326
Swathi Kulkarni <swathik012004@gmail.com>
2024-06-07 19:55:01 +0530

    some

--- patient_details/models.py
+++ patient_details/models.py
@@ -6,7 +6,9 @@ GENDER = (
     ("female", "Female")
 )
 
+
 class PatientData(models.Model):
+    patient_id = models.CharField(max_length=10, primary_key=True, default="NULL")
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     dob = models.DateField()
@@ -15,6 +17,7 @@ class PatientData(models.Model):
     email = models.EmailField()
     emergency_contact_num = models.IntegerField(max_length=10)
     govt_id = models.CharField(max_length=50)
+    symptoms = models.TextField(max_length=400, default="NOTHING")
 
     def __str__(self):
         self.first_name
